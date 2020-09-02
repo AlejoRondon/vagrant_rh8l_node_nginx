79d559556fa8a013a380f8d1104fb4f130b4149a
Alejandro Rondón <blackphotografy@gmail.com>
2020-09-02 17:46:12 -0500

    [ADD] startup_firewall.sh

---
+++ startup_firewall.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo "<¬¬¬¬¬>STARTING UP FIREWALLD<¬¬¬¬¬>"
+echo "https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-using-firewalld-on-centos-8-es"
+
+firewall-cmd --zone=public --permanent --add-service=http
+firewall-cmd --zone=public --permanent --add-service=https
+ 
+sudo firewall-cmd --zone=public --permanent --add-port=80/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=3000-3100/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=5000-5100/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=9000-9100/tcp
+
+echo "<¬¬¬¬¬>FIREWALLD STATE>"
+firewall-cmd --state
+echo "<¬¬¬¬¬>FIREWALLD RELOAD"
+firewall-cmd --reload
+echo "<¬¬¬¬¬>FIREWALLD OPEN PORTS"
+firewall-cmd --zone=public --list-ports --permanent
+echo "<¬¬¬¬¬>FIREWALLD SERVICES"
+firewall-cmd --zone=public --list-services --permanent
+echo "<¬¬¬¬¬>FIREWALLD DEFAULT ZONE>"
+firewall-cmd --get-default-zone
+echo "<¬¬¬¬¬>FIREWALLD ACTIVE ZONE>"
+firewall-cmd --get-active-zones
+echo "<¬¬¬¬¬>FIREWALLD LIST>"
+firewall-cmd --list-all
