400396fa3e8a39f5a0bfbe446819bef82d496173
Alejandro Rondón <blackphotografy@gmail.com>
2020-09-02 17:46:44 -0500

    [ADD] startup_nginx.sh

---
+++ startup_nginx.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "<------>STARTING UP NGINX<------>"
+echo "<------>https://www.linode.com/docs/development/nodejs/how-to-install-nodejs-and-nginx-on-centos-8/"
+echo "<------>https://linuxconfig.org/install-nginx-on-redhat-8"
+echo "<------>https://www.cyberciti.biz/faq/how-to-install-and-use-nginx-on-centos-7-rhel-7/"
+# Install NGINX, the command line utilities tmux, and tar.
+echo "<------>Installing NGINX"
+dnf update
+dnf install -y nginx
+# Start the Nginx service:
+echo "<------>Starting NGINX"
+systemctl start nginx
+echo "<------>Enablig NGINX"
+systemctl enable nginx
+echo "<------>Checking NGINX response"
+curl http://localhost/
+echo "<------>Creating NGINX directories"
+mkdir -p /etc/nginx/{sites-available,sites-enabled}
+mkdir -p /var/www
