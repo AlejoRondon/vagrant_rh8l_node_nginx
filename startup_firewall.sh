6a9d6a7ca051867f4c2380c1f17872f062915f33
Alejandro Rondón <blackphotografy@gmail.com>
2020-09-04 16:47:23 -0500

    [EDIT] Enabling and starting firewalld in startup_firewall.sh

--- startup_firewall.sh
+++ startup_firewall.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 echo "<¬¬¬¬¬>STARTING UP FIREWALLD<¬¬¬¬¬>"
 echo "https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-using-firewalld-on-centos-8-es"
-
+systemctl enable firewalld
+systemctl start firewalld
 firewall-cmd --zone=public --permanent --add-service=http
 firewall-cmd --zone=public --permanent --add-service=https
  
-sudo firewall-cmd --zone=public --permanent --add-port=80/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=3000-3100/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=5000-5100/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=9000-9100/tcp
+firewall-cmd --zone=public --permanent --add-port=80/tcp
+firewall-cmd --zone=public --permanent --add-port=3000-3100/tcp
+firewall-cmd --zone=public --permanent --add-port=5000-5100/tcp
+firewall-cmd --zone=public --permanent --add-port=9000-9100/tcp
 
 echo "<¬¬¬¬¬>FIREWALLD STATE>"
 firewall-cmd --state
